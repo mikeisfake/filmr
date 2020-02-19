6460b4a1fbff549bad61f249cea6e50e89f985a0
mikeisfake <mikescooper@icloud.com>
2020-02-19 09:43:17 -0500

    deleted some watchlist stuff to come back to later possibly.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,6 @@
 class Movie < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
-  has_many :watchlists, through: :movie_watchlists
 
   validates :imdbid, presence: true
 
--- app/models/movie_watchlist.rb
+++
@@ -1,4 +0,0 @@
-class MovieWatchlist < ApplicationRecord
-  belongs_to :movie
-  belongs_to :watchlist
-end
