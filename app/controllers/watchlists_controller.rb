3ad20cb8d7e66fca13e47ba774ce247d3edb00db
mikeisfake <mikescooper@icloud.com>
2020-02-21 03:25:06 -0500

    stubbed out working logic for adding movie to watchlist

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -3,16 +3,19 @@ class WatchlistsController < ApplicationController
   def create
 
     @movie = Movie.find(params[:movie_id])
-
     if current_user.watchlist.nil?
-      @watchlist = Watchlist.create
+      @watchlist = Watchlist.new
       current_user.watchlist = @watchlist
       @watchlist.movies << @movie
+      if @watchlist.save
+        redirect_to user_path(current_user)
+      end
     else
-      @watchlist = current_user.watchlist
-      @watchlist.movies << @movie
+      if !current_user.watchlist.movies.include?(@movie)
+        current_user.watchlist.movies << @movie
+        redirect_to root_path
+      end
     end
-    byebug 
   end
 
 
